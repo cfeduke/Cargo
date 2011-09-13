d248697548a574b8752d408f7864ddd214f5388d
Charles Feduke <charles.feduke@gmail.com>
2011-09-13 18:27:48 -0400

    cargo view can now be moved

--- Classes/CarDriver.m
+++ Classes/CarDriver.m
@@ -13,14 +13,17 @@
 -(IBAction)buttonClicked:(id)sender {
     UIButton *button = (UIButton *)sender;
     switch (button.tag) {
-        case 1:
-            
+        case 1: // up
+            cargoView.center = CGPointMake(cargoView.center.x, cargoView.center.y - 10);
             break;
-        case 2:
+        case 2: // down
+            cargoView.center = CGPointMake(cargoView.center.x, cargoView.center.y + 10);
             break;
-        case 3:
+        case 3: // right
+            cargoView.center = CGPointMake(cargoView.center.x + 10, cargoView.center.y);
             break;
-        case 4:
+        case 4: // left
+            cargoView.center = CGPointMake(cargoView.center.x - 10, cargoView.center.y);
             break;
         default:
             break;
